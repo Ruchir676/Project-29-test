45b290d1daee1b931a8246297022a3ee58fa4282
Ruchir676 <72313742+Ruchir676@users.noreply.github.com>
2020-11-28 23:24:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+var a;
+var circles=[];
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+  
+ // camera=new Camera(width/2,height/2,0.5);
+  //camera.on();
+  a=height;
+  circles.push(width/2)
+
+  box1 = new Box(300,275,30,40);
+  box2 = new Box(330,275,30,40);
+  box3 = new Box(360,275,30,40);
+  box4 = new Box(390,275,30,40);
+  box5 = new Box(420,275,30,40);
+  box6 = new Box(450,275,30,40);
+  box7 = new Box(480,275,30,40);
+  box8 = new Box(330,235,30,40);
+  box9 = new Box(360,235,30,40);
+  box10 = new Box(390,235,30,40);
+  box11 = new Box(420,235,30,40);
+  box12 = new Box(450,235,30,40);
+  box13 = new Box(360,195,30,40);
+  box14 = new Box(390,195,30,40);
+  box15 = new Box(420,195,30,40);
+  box16 = new Box(390,155,30,40);
+}
+
+function draw() {
+  //camera.zoom=camera.zoom+1
+  background(0);  
+  
+  
+  a=a-1;
+  //camera.zoom=camera.zoom+0.01
+ //camera.position={x:width/2,y:a}
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+ // camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+
+ box1.display();
+ box2.display();
+ box3.display();
+ box4.display();
+ box5.display();
+ box6.display();
+ box7.display();
+ box8.display();
+ box9.display();
+ box10.display();
+ box11.display();
+ box12.display();
+ box13.display();
+ box14.display();
+ box15.display();
+ box16.display();
+
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      camera.position.x=camera.position.x+10;
+    }
+  }
+} 
